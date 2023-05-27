54526ece47282009839d16b2a7f7e89f7234d42d
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-05-27 20:48:34 +0200

    add shuffle function for cardStack

--- RingOfFire/src/app/game/game.component.ts
+++ RingOfFire/src/app/game/game.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Game } from 'src/modules/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string | undefined;
+  game: Game = new Game;
+
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game)
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 }
