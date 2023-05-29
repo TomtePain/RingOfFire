44b55cfd934706b9cc7d9ca2cd2f3f4de09d89a0
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-05-29 12:57:22 +0200

    add a playerbar

--- RingOfFire/src/app/app-routing.module.ts
+++ RingOfFire/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
   { path: 'game', component: GameComponent },
+  { path: 'player', component: PlayerComponent },
 ];
 
 @NgModule({
---
+++ RingOfFire/src/app/player/player.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Game } from 'src/modules/game';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss']
+})
+export class PlayerComponent extends Game{
+
+  @Input() name: any;
+
+}
